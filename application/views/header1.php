06ee394b36417280cb4d721f77f8e67679df4b0c
Usaid Chaq <usaidelqudsy.chaq20@gmail.com>
2016-07-27 15:10:56 +0700

    tambah favicon

--- application/views/header1.php
+++ application/views/header1.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Aspirasi</title>
+    <link rel="shortcut icon" href="<?php  echo base_url(); ?>assets/images/favicon.ico">
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
